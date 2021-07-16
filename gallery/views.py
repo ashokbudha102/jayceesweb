16b1c85bcec32d85e48380b5ff4ff221fd2896c3
ashok.budha2015 <ashok.budha2015@gmail.com>
2021-07-16 19:14:40 +0545

    gallery changed

--- gallery/views.py
+++ gallery/views.py
@@ -3,4 +3,4 @@ from .models import gallery_images
 # Create your views here.
 def gallery(request):
     images = gallery_images.objects.all().order_by('-id')
-    return render(request,'gallery.html',{'gallery':images})
+    return render(request,'gallery.html',{'images':images})
